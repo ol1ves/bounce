3cd0d22e749049ba810b8f07c31c83e464c1e0ce
QV Productions <ol1veso1led@gmail.com>
2022-11-09 21:36:05 -0800

    add dvd_logo sprite

---
+++ src/gfx/dvd_logo.h
@@ -0,0 +1,18 @@
+#ifndef dvd_logo_include_file
+#define dvd_logo_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define dvd_logo_width 16
+#define dvd_logo_height 16
+#define dvd_logo_size 258
+#define dvd_logo ((gfx_sprite_t*)dvd_logo_data)
+extern unsigned char dvd_logo_data[258];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
