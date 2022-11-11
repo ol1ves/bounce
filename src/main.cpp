b22b580992a93c0765c6f7d76129c5eefdd21610
ol1ves <103908970+ol1ves@users.noreply.github.com>
2022-11-11 12:20:40 -0800

    Bounce DVD logo

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,28 @@
 #include <graphx.h>
 #include <stdlib.h>
 
+#include <sys/util.h>
+#include <sys/rtc.h>
+#include <sys/timers.h>
 #include "gfx/gfx.h"
 
 void begin();
 void end();
 bool step();
-void draw();
+void draw(int posX, int posY);
+void collide();
+
+static int LOGO_WIDTH = 56, LOGO_HEIGHT = 30; 
+
+
 
 int logoPos[2] {0, 0};
+int logoVel[2] {1, 1};
 
 int main(void)
 {
+    gfx_SetDrawBuffer();
+
     // Begin
     begin();
     
@@ -22,11 +33,21 @@ int main(void)
 
     // While no keypress
     while (!step()) {
+
+        logoPos[0] += logoVel[0];
+        logoPos[1] += logoVel[1];
+
+        collide();
+
+        gfx_ZeroScreen();
+
         // Draw the current frame
-        draw();
+        draw(logoPos[0], logoPos[1]);
 
         // Queue the current frame for display
         gfx_SwapDraw();
+
+        sleep(0.1);
     }
     
     // Exit palletized mode and exit
@@ -47,6 +68,11 @@ bool step() {
     return os_GetCSC();
 }
 
-void draw() {
-    gfx_TransparentSprite(dvd_logo, logoPos[0], logoPos[1]);
+void draw(int posX, int posY) {
+    gfx_TransparentSprite(dvd_logo, posX, posY);
+}
+
+void collide() {
+    if (logoPos[0] < 0 || logoPos[0] + LOGO_WIDTH > GFX_LCD_WIDTH) logoVel[0] *= -1;
+    if (logoPos[1] < 0 || logoPos[1] + LOGO_HEIGHT > GFX_LCD_HEIGHT) logoVel[1] *= -1;
 }
